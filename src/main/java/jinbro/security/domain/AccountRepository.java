6ed8e8f4129fcb51c1916fd8ff5b4c405fbc34b4
imjinbro <jinbro@likelion.org>
2018-07-17 15:02:13 +0900

    create repository

---
+++ src/main/java/jinbro/security/domain/AccountRepository.java
@@ -0,0 +1,11 @@
+package jinbro.security.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    Optional<Account> findByUserId(String userId);
+    Optional<Account> findBySocialId(Long socialId);
+}
