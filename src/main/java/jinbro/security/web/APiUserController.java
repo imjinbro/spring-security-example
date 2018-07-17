0b00008a07bf7d65c6836468dc5251d5c333482f
imjinbro <jinbro@likelion.org>
2018-07-17 14:27:38 +0900

    set dev-env

---
+++ src/main/java/jinbro/security/web/APiUserController.java
@@ -0,0 +1,15 @@
+package jinbro.security.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class APiUserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
